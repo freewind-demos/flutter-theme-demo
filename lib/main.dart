f59743395ddc31bc091fe9090438fc812cb5e892
Freewind <nowindlee@gmail.com>
2026-03-14 22:11:17 +0800

    flutter-theme-demo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// Flutter 主题
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Theme')),
+        body: Center(child: Text('Themed App')),
+      ),
+    );
+  }
+}
